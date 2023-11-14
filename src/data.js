c6a311a077b0357d3a057a404f19d12c6e6cddf1
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-11-14 19:54:09 +0000

    modified:   package-lock.json 	modified:   src/App.js 	modified:   src/components/Features.js 	modified:   src/components/FeaturesSecond.js 	modified:   src/components/Footer.js 	modified:   src/components/Header.js 	modified:   src/components/Hero.js 	modified:   src/components/NavMobile.js 	modified:   src/components/NewItems.js 	modified:   src/components/NewItemsSlider.js 	modified:   src/components/Newsletter.js 	modified:   src/components/ProductSlider.js 	modified:   src/components/Products.js 	modified:   src/components/Stats.js 	modified:   src/components/Testimonial.js 	modified:   src/components/TestimonialSlider.js 	modified:   src/data.js

--- src/data.js
+++ src/data.js
@@ -52,7 +52,7 @@ export const navigation = [
 ];
 
 export const hero = {
-  title: 'Creative Home Simpify your Furniture',
+  title: 'Moulding your Dreams into Reality',
   subtitle:
     'Do i have consent to record this meeting gain locaion, root-and-branch, review, nor game plan who’s the goto',
   buttonText: 'Shop Now',
